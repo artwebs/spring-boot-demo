c51b0319d985cc8ed177a6d40928c57eed11b7b6
artwebs <artwebs@126.com>
2017-08-31 17:59:21 +0800

    update

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SpringController{
-  @RequestMapping("/")
+  @RequestMapping("/home")
   @ResponseBody
   String home(){
     return "Hello world";
