c78cbb6f1354bff90cfc88de5fb4f9962a551c4e
artwebs <artwebs@126.com>
2017-08-31 16:59:05 +0800

    update

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@EnableAutoConfiguration
 public class SpringController{
   @RequestMapping("/")
   @ResponseBody
@@ -14,7 +13,4 @@ public class SpringController{
     return "Hello world";
   }
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(SpringController.class,args);
-  }
 }
